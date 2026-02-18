c67e5867d18a514ab17b62cabc599e2b15db9a52
ryryashy7 <rileyashton2109@outlook.com>
2026-02-18 00:17:41 +0000

    finished activity 3

---
+++ lesson1/act3.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+hours = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+print(hours)
+
+print('Add 2 hours:', hours + 2)
+print('Average study time:', np.mean(hours))
+
+import matplotlib.pyplot as plt
+
+scores = np.array([50, 55, 65, 70, 79, 84, 87, 90, 92, 95])
+
+
+
+from sklearn.linear_model import LinearRegression
+x = hours.reshape(-1, 1)
+y = scores
+
+model = LinearRegression()
+model.fit(x, y) 
+
+prediction = model.predict([[6]])
+print("Predicted score for 6 hours of study:", prediction)
+
+plt.scatter(x,y)
+plt.plot(x, model.predict(x), color='red')
+plt.xlabel('Hours Studied')
+plt.ylabel('Test Score')
+plt.title('Study Time vs Test Score')
+plt.show()
