1ae2a81b0d13c2559bc29b9a0507dae7a2e74867
ryryashy7 <rileyashton2109@outlook.com>
2026-02-19 02:40:08 +0000

    feat, bug; font changes, colour changes

--- lesson1/act2.py
+++ lesson1/act2.py
@@ -8,7 +8,7 @@ print('Average study time:', np.mean(hours))
 
 import matplotlib.pyplot as plt
 
-scores = np.array([50, 55, 65, 70, 80, 96, 97, 89, 91, 93])
+scores = np.array([125, 55, 65, 70, 80, 96, 97, 89, 91, 93])
 
 plt.scatter(hours, scores)
 plt.xlabel('Hours Studied')
